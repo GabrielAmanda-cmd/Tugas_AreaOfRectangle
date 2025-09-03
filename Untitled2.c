e82bf6fbb54712dff548b87b6ed66aba9304a4b9
GabrielAmanda-cmd <gabylim008@gmail.com>
2025-09-03 11:23:32 +0700

    Create Untitled2.c

---
+++ Untitled2.c
@@ -0,0 +1,9 @@
+//tugas 2
+int main() {
+    int width, length, area;
+    scanf("%d %d", &width, &length);
+    area = width*length;
+    printf("%d x %d = %d", width, length, area);
+
+    return 0;
+}
